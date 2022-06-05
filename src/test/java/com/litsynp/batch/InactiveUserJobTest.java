1d368db87a28168cb128bd88cde9bb7dfb3c466f
litsynp <nocte55is@gmail.com>
2022-06-06 02:47:26 +0900

    feat: add MySQL and use H2 for test

--- src/test/java/com/litsynp/batch/InactiveUserJobTest.java
+++ src/test/java/com/litsynp/batch/InactiveUserJobTest.java
@@ -13,11 +13,14 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2) // MySQL for real, H2 for test
 class InactiveUserJobTest {
 
     @Autowired
